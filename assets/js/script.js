1131a033208d33ae12908f0049fe85112dcf8a99
MilenaCode <sahni.milena@gmail.com>
2022-08-11 20:22:27 +0000

    create header with hamburger navigation and logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+function toggleNav() {
+    var x = document.getElementById("myNav");
+    const menuBtn = document.querySelector('.menu-btn');
+    let menuOpen = false;
+  
+    if (x.style.height === "100%") {
+      x.style.height = "0";
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+    } else {
+      x.style.height = "100%";
+      menuBtn.classList.add('open');
+      menuOpen = true;
+    }
+  
+  
+  }
