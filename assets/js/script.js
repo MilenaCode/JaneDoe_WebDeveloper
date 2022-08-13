1e052a9d7397bb16606b1aaca42ba7d9bf871819
MilenaCode <sahni.milena@gmail.com>
2022-08-13 16:32:13 +0000

    create button to scroll to top of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,5 +13,24 @@ function toggleNav() {
       menuOpen = true;
     }
   
-  
   }
+
+  //Get the button
+var mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.opacity = "0";
+  }
+}
+  
+  // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
